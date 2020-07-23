f0cd8c9497142aa68546130010b4a38147d33b73
kdawg <mcleod.karla@gmail.com>
2020-07-23 14:36:01 -0400

    added put to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,4 +13,17 @@ router.get('/workouts', (req, res) => {
         });
 });
 
+router.put('/workouts/:id', (req, res) => {
+    db.Workout.updateOne(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } }
+    )
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        });
+});
+
 module.exports = router;
