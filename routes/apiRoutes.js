50cf771dc1fa3bc8cbf2aabc89a63cea09d1553e
kdawg <mcleod.karla@gmail.com>
2020-07-23 14:32:36 -0400

    added get workouts in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,15 @@ const router = require('express').Router();
 
 const db = require('../models');
 
+router.get('/workouts', (req, res) => {
+    db.Workout.findOne({}, {}, { sort: { day: -1 } })
+        .then((dbWorkout) => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        });
+});
+
 module.exports = router;
